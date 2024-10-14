2c85e24d22b67ed66f05909ad45bdabcf2101f43
marsodeb <marceaud.pro@gmail.com>
2024-10-14 18:48:30 +0200

    training on navbar

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,12 +2,15 @@ import logo from '../../assets/logo.png';
 
 function Header() {
     return (
-        <header className="shadow-main p-4 rounded-lg w-11/12 mx-auto my-5 bg-nav-bar-color">
+        <header className="p-4 rounded-lg w-11/12 mx-auto my-5 bg-nav-color">
             <nav className="container mx-auto flex justify-between items-center">
-                <img src={logo} alt="logo" className="h-10"></img>
-                <ul className="flex text-white w-5/12 justify-between font-medium">
-                    <li>HOME</li>
-                    <li>PROJECT</li>
+                <div className='flex items-center gap-5'>
+                    <img src={logo} alt="logo" className="h-10"></img>
+                    <h1 className='text-white bg-nav-color py-2 px-4 rounded-lg font-medium'>TAILWIND TRANING</h1>
+                </div>
+                <ul className="flex gap-10 text-white justify-between">
+                    <li className='nav ease-out duration-300'>MARSODEB</li>
+                    <li className='nav ease-out duration-300'>NAVBAR</li>
                 </ul>
             </nav>
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        'nav-bar-color': 'rgba(255, 255, 255, 0.1)',
+        'nav-color': 'rgba(255, 255, 255, 0.1)',
+        'nav-color-hover': 'rgba(255, 255, 255, 0.2)',
       },
       boxShadow: {
         'main': '0px 0px 10px rgba(255, 255, 255, 0.1)',
